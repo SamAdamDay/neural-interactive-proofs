7660a987bdfcd0d162f71348bc9b85c432e88b31
Sam Adam-Day <me@samadamday.com>
2024-01-16 21:34:33 +0000

    Created test for graph isomorphism env step

--- tests/test_environments.py
+++ tests/test_environments.py
@@ -1,6 +1,18 @@
+import torch
+from torch.testing import assert_close
+
+from tensordict import TensorDict
+
 from torchrl.envs.utils import check_env_specs
 
-from pvg.parameters import Parameters, ScenarioType, TrainerType
+from pvg.parameters import (
+    Parameters,
+    ScenarioType,
+    TrainerType,
+    AgentsParameters,
+    GraphIsomorphismAgentParameters,
+    PpoParameters,
+)
 from pvg.experiment_settings import ExperimentSettings
 from pvg.graph_isomorphism.environment import GraphIsomorphismEnvironment
 
@@ -15,3 +27,111 @@ def test_environment_specs():
         settings = ExperimentSettings(device="cpu", test_run=True)
         env = environment_class(params, settings)
         check_env_specs(env)
+
+
+def test_graph_isomorphism_environment_step():
+    """Make sure the GI environment step method works as expected."""
+
+    batch_size = 12
+    num_nodes = batch_size // 2
+    max_message_rounds = 6
+
+    # Set up the environment.
+    params = Parameters(
+        ScenarioType.GRAPH_ISOMORPHISM,
+        TrainerType.PPO,
+        "test",
+        ppo=PpoParameters(frames_per_batch=batch_size * max_message_rounds),
+        max_message_rounds=max_message_rounds,
+        agents=AgentsParameters(
+            [
+                ("prover", GraphIsomorphismAgentParameters()),
+                ("verifier", GraphIsomorphismAgentParameters()),
+            ]
+        ),
+        prover_reward=1,
+        verifier_reward=2,
+        verifier_terminated_penalty=-4,
+    )
+    settings = ExperimentSettings(device="cpu", test_run=True)
+    env = GraphIsomorphismEnvironment(params, settings)
+
+    # Set up the TensorDict to feed into the environment.
+    env_td = TensorDict(
+        dict(
+            adjacency=torch.zeros(batch_size, num_nodes, num_nodes, dtype=torch.int32),
+            node_mask=torch.ones(batch_size, num_nodes, dtype=torch.bool),
+            round=torch.remainder(
+                torch.arange(batch_size, dtype=torch.int32), max_message_rounds
+            ),
+            x=torch.zeros(
+                batch_size, 2, num_nodes, max_message_rounds, dtype=torch.float32
+            ),
+            y=torch.tensor([0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1]),
+            agents=dict(
+                node_selected=torch.stack(
+                    [torch.arange(batch_size), torch.arange(batch_size - 1, -1, -1)]
+                ).transpose(0, 1),
+                decision=torch.tensor(
+                    [[0] * batch_size, [0, 0, 1, 1, 2, 2, 0, 0, 1, 1, 1, 1]]
+                ).transpose(0, 1),
+            ),
+            done=torch.tensor([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0]),
+        ),
+        batch_size=batch_size,
+    )
+
+    # Define the expected output.
+    expected_x = torch.zeros(
+        batch_size, 2, num_nodes, max_message_rounds, dtype=torch.float32
+    )
+    expected_message = torch.zeros(batch_size, dtype=torch.int64)
+    for i in range(batch_size):
+        round = env_td["round"][i]
+        agent_index = round % 2
+        message = env_td["agents", "node_selected"][i, agent_index]
+        expected_message[i] = message
+        graph_id = message // num_nodes
+        expected_x[i, graph_id, message % num_nodes, round] = 1
+    expected_next = TensorDict(
+        dict(
+            adjacency=env_td["adjacency"],
+            node_mask=env_td["node_mask"],
+            x=expected_x,
+            round=env_td["round"] + 1,
+            done=torch.tensor([0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1]),
+            message=expected_message,
+            agents=dict(
+                reward=torch.tensor(
+                    [
+                        [0, 0],
+                        [0, 2],
+                        [0, 0],
+                        [1, 2],
+                        [0, 0],
+                        [0, -4],
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                        [1, 0],
+                        [0, 0],
+                        [1, 2],
+                    ],
+                    dtype=torch.float32,
+                )
+            ),
+        ),
+        batch_size=batch_size,
+    )
+
+    # Run the step method.
+    next = env._step(env_td)
+
+    # Check that the output is as expected.
+    assert_close(next["adjacency"], expected_next["adjacency"])
+    assert_close(next["node_mask"], expected_next["node_mask"])
+    assert_close(next["x"], expected_next["x"])
+    assert_close(next["round"], expected_next["round"])
+    assert_close(next["done"], expected_next["done"])
+    assert_close(next["message"], expected_next["message"])
+    assert_close(next["agents", "reward"], expected_next["agents", "reward"])
