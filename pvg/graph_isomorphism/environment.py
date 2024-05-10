Fixed bugs in observation normalisation and env step

pvg/graph_isomorphism/environment.py
@@ -216,6 +216,15 @@ class GraphIsomorphismEnvironment(Environment):
             self._max_num_nodes = self.dataset["x"].shape[-2]
         return self._max_num_nodes
 
+    @property
+    def _message_history_shape(self) -> tuple:
+        return (
+            self.num_envs,
+            2,
+            self.max_num_nodes,
+            self.protocol_handler.max_message_rounds,
+        )
+
     def _get_observation_spec(self) -> CompositeSpec:
         """Get the specification of the agent observations.
 
@@ -247,17 +256,6 @@ class GraphIsomorphismEnvironment(Environment):
                 self.max_num_nodes,
             ),
             dtype=torch.bool,
-            device=self.device,
-        )
-        base_observation_spec["x"] = DiscreteTensorSpec(
-            self.protocol_handler.max_message_rounds,
-            shape=(
-                self.num_envs,
-                2,
-                self.max_num_nodes,
-                self.protocol_handler.max_message_rounds,
-            ),
-            dtype=torch.float,
             device=self.device,
         )
         base_observation_spec["message"] = DiscreteTensorSpec(
@@ -295,7 +293,7 @@ class GraphIsomorphismEnvironment(Environment):
         )
         return base_action_spec
 
-    def _compute_x_and_message(
+    def _compute_message_history(
         self,
         env_td: TensorDictBase,
         next_td: TensorDictBase,
@@ -319,7 +317,9 @@ class GraphIsomorphismEnvironment(Environment):
         """
 
         # Extract the tensors from the dict
-        x: Float[Tensor, "... graph node message_round"] = env_td["x"]
+        message_history: Float[Tensor, "... graph node message_round"] = env_td[
+            "message_history"
+        ]
         round: Int[Tensor, "..."] = env_td["round"]
         node_selected: Int[Tensor, "... agent"] = env_td["agents", "node_selected"]
 
@@ -339,10 +339,12 @@ class GraphIsomorphismEnvironment(Environment):
 
         # Insert the message into the message history at the current round
         round_mask = F.one_hot(round, self.protocol_handler.max_message_rounds).bool()
-        x.masked_scatter_(round_mask[..., None, None, :], message)
+        message_history = message_history.masked_scatter(
+            round_mask[..., None, None, :], message
+        )
 
         # Add the message history and next message to the next tensordict
-        next_td["x"] = x
+        next_td["message_history"] = message_history
         next_td["message"] = message
 
         return next_td
@@ -373,6 +375,9 @@ class GraphIsomorphismEnvironment(Environment):
         env_td["adjacency"][mask] = data_batch["adjacency"]
         env_td["node_mask"][mask] = data_batch["node_mask"]
         env_td["y"][mask] = data_batch["y"].unsqueeze(-1)
+        env_td["message_history"][mask] = torch.zeros_like(
+            env_td["message_history"][mask]
+        )
         env_td["x"][mask] = torch.zeros_like(env_td["x"][mask])
         env_td["message"][mask] = 0
         env_td["round"][mask] = 0

pvg/scenario_base/environment.py
@@ -4,7 +4,6 @@ from abc import ABC, abstractmethod
 from typing import Optional
 
 import torch
-from torch import Tensor
 
 from tensordict.tensordict import TensorDict, TensorDictBase
 
@@ -17,11 +16,9 @@ from torchrl.data.tensor_specs import (
 )
 from torchrl.envs import EnvBase
 
-from jaxtyping import Float, Int, Bool
-
 from pvg.scenario_base import DataLoader, Dataset
 from pvg.protocols import ProtocolHandler
-from pvg.parameters import Parameters, InteractionProtocolType, AGENT_NAMES
+from pvg.parameters import Parameters
 from pvg.experiment_settings import ExperimentSettings
 from pvg.utils.data import VariableDataCycler
 
@@ -107,6 +104,19 @@ class Environment(EnvBase, ABC):
         self.reward_spec = self._get_reward_spec()
         self.done_spec = self._get_done_spec()
 
+    @property
+    @abstractmethod
+    def _message_history_shape(self) -> tuple:
+        """Get the shape of the message history and 'x' tensors
+
+        This is used to make the specification for these.
+
+        Returns
+        -------
+        message_history_shape: tuple
+            The common shape of the message history and 'x' tensors.
+        """
+
     @abstractmethod
     def _get_observation_spec(self) -> TensorSpec:
         """Get the specification of the agent observations.
@@ -143,6 +153,17 @@ class Environment(EnvBase, ABC):
                 3,
                 shape=(self.num_envs,),
                 dtype=self._int_dtype,
+                device=self.device,
+            ),
+            x=UnboundedContinuousTensorSpec(
+                shape=self._message_history_shape,
+                dtype=torch.float,
+                device=self.device,
+            ),
+            message_history=BinaryDiscreteTensorSpec(
+                self._message_history_shape[-1],
+                shape=self._message_history_shape,
+                dtype=torch.float,
                 device=self.device,
             ),
             shape=(self.num_envs,),
@@ -268,11 +289,20 @@ class Environment(EnvBase, ABC):
 
         # The observations are passed through unchanged
         for key in self.observation_spec.keys():
-            if key not in ["x", "message", "round", "decision_restriction"]:
+            if key not in [
+                "x",
+                "message_history",
+                "message",
+                "round",
+                "decision_restriction",
+            ]:
                 next_td[key] = env_td[key]
 
         # Compute the message history and next message
-        next_td = self._compute_x_and_message(env_td, next_td)
+        next_td = self._compute_message_history(env_td, next_td)
+
+        # Clone the message history to the 'x' feature tensor
+        next_td["x"] = next_td["message_history"].clone()
 
         # Compute the done signal and reward
         done, reward = self.protocol_handler.step_interaction_protocol(env_td)
@@ -286,12 +316,12 @@ class Environment(EnvBase, ABC):
         return next_td
 
     @abstractmethod
-    def _compute_x_and_message(
+    def _compute_message_history(
         self,
         env_td: TensorDictBase,
         next_td: TensorDictBase,
     ) -> TensorDictBase:
-        """Compute the message history and next message.
+        """Compute the new message history and next message.
 
         Used in the `_step` method of the environment.
 

pvg/trainers/rl_trainer_base.py
@@ -117,6 +117,11 @@ class ReinforcementLearningTrainer(Trainer, ABC):
 
         # Add the observation normalization transforms if requested
         if self.params.rl.normalize_observations:
+
+            # Set the seed before computing the normalization statistics
+            torch.manual_seed(self.params.seed)
+            np.random.seed(self.params.seed)
+
             self.train_environment = TransformedEnv(self.train_environment)
             self.test_environment = TransformedEnv(self.test_environment)
 
@@ -126,15 +131,18 @@ class ReinforcementLearningTrainer(Trainer, ABC):
                 num_normalization_steps = 1
             else:
                 num_normalization_steps = self.params.rl.num_normalization_steps
+            pbar = self.settings.tqdm_func(total=4, desc="Computing norm stats")
             for env in [self.train_environment, self.test_environment]:
                 for key, size in zip(["x", "message"], [4, 3]):
-                    transform = ObservationNorm(in_keys=[key])
+                    transform = ObservationNorm(in_keys=[key], standard_normal=True)
                     env.append_transform(transform)
                     transform.init_stats(
                         num_normalization_steps,
                         cat_dim=-size,
                         reduce_dim=list(range(-size - 1, 0)),
                     )
+                    pbar.update(1)
+            pbar.close()
 
     def train(self):
         """Train the agents."""