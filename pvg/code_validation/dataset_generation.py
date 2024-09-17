c3d095d3c06d85efe36fbcfa10368654394c2e0d
Lewis Hammond <lewis@lewishammond.com>
2024-09-17 07:13:31 +0100

    Update dataset generation procedure

--- pvg/code_validation/dataset_generation.py
+++ pvg/code_validation/dataset_generation.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 from datasets import load_dataset
 from evaluate import load
 import re
+import textdistance
 
 from apps_metric import utils
 
@@ -38,33 +39,66 @@ class CodeValidationDatasetConfig:
     split: str | None = None
     fraction_to_modify: float = 0.5
     remove_original: bool = True
+    num_problematic_inputs: int = 3
+    system_prompt: Optional[str] = None
     # system_prompt: Optional[str] = "I am developing a new code validation system. In order test this system, I need to provide it with a set of correct and incorrect solutions for a given coding problem. I already have the correct solutions, and I need your help to generate some incorrect solutions by modifying the correct solutions. In what follows, I will provide you with a QUESTION and a SOLUTION. Your task is to subtly modify this solution by introducing an error that causes the code to fail on a small fraction of inputs. To test the system thoroughly, the error in this MODIFIED SOLUTION should be as difficult to spot as possible (while making sure the code remains similar to the original solution) and should not be a simple syntax error. Please only modify the solution given to you and preserve all other aspects of the solution (such as the formatting and variable names). Do not include any natural language explanation of your modifications or any comments in the code. In other words, please only include the modified code itself in your response."
-    system_prompt: Optional[str] = (
-        "I am developing a new code validation system. In order test this system, I need to provide it with a set of correct and incorrect solutions for a given coding problem. I already have the correct solutions, and I need your help to generate some incorrect solutions by modifying the correct solutions. In what follows, I will provide you with a QUESTION and a SOLUTION. Your task is to subtly modify this solution by introducing an error that causes the code to fail on a small fraction of inputs, and to provide a PROBLEMATIC INPUT on which the original solution works, but your MODIFIED SOLUTION fails. To test the system thoroughly, the error in this MODIFIED SOLUTION should be as difficult to spot as possible (while making sure the code remains similar to the original solution) and should not be a simple syntax error. Please only modify the solution given to you and preserve all other aspects of the solution (such as the formatting and variable names). Do not include any natural language explanation of your modifications or any comments in the code. The format of your answer should be:\
-    \n\
-    \n\
-    MODIFIED SOLUTION:\n\
-    ```<your modified code here>```\n\n\
-    PROBLEMATIC INPUT: <your problematic input here>"
-    )
+
+    def __post_init__(self):
+
+        if self.system_prompt is None:
+            if self.num_problematic_inputs < 0 or self.num_problematic_inputs > 10:
+                return ValueError("num_problematic_inputs must be between 0 and 10")
+            else:
+                indices = {
+                    1: "first",
+                    2: "second",
+                    3: "third",
+                    4: "fourth",
+                    5: "fifth",
+                    6: "sixth",
+                    7: "seventh",
+                    8: "eighth",
+                    9: "ninth",
+                    10: "tenth",
+                }
+                problematic_inputs = "\n\n".join(
+                    [
+                        f"PROBLEMATIC INPUT {i+1}:\n<your {indices[i+1]} problematic input here>"
+                        for i in range(self.num_problematic_inputs)
+                    ]
+                )
+                self.system_prompt = f"I am developing a new code validation system. In order test this system, I need to provide it with a set of correct and incorrect solutions for a given coding problem. I already have the correct solutions, and I need your help to generate some incorrect solutions by modifying the correct solutions. In what follows, I will provide you with a QUESTION and a SOLUTION. Your task is to subtly modify this solution by introducing an error that causes the code to fail on a small fraction of inputs, and to provide {self.num_problematic_inputs} PROBLEMATIC INPUTs on which the original solution *works*, but your MODIFIED SOLUTION *fails*. To test the system thoroughly, the error in this MODIFIED SOLUTION should be as difficult to spot as possible (while making sure the code remains similar to the original solution) and should not be a simple syntax error. Please only modify the solution given to you and preserve all other aspects of the solution (such as the formatting and variable names). You must *not* include any natural language explanation of your modifications or any comments in the code. The format of your answer should be:\n\n MODIFIED SOLUTION:\n ```<your modified code here>```\n\n{problematic_inputs}\n"
+
+    # system_prompt: Optional[str] = (
+    #     "I am developing a new code validation system. In order test this system, I need to provide it with a set of correct and incorrect solutions for a given coding problem. I already have the correct solutions, and I need your help to generate some incorrect solutions by modifying the correct solutions. In what follows, I will provide you with a QUESTION and a SOLUTION. Your task is to subtly modify this solution by introducing an error that causes the code to fail on a small fraction of inputs, and to provide three PROBLEMATIC INPUTs on which the original solution *works*, but your MODIFIED SOLUTION *fails*. To test the system thoroughly, the error in this MODIFIED SOLUTION should be as difficult to spot as possible (while making sure the code remains similar to the original solution) and should not be a simple syntax error. Please only modify the solution given to you and preserve all other aspects of the solution (such as the formatting and variable names). You must *not* include any natural language explanation of your modifications or any comments in the code. The format of your answer should be:\
+    # \n\
+    # \n\
+    # MODIFIED SOLUTION:\n\
+    # ```<your modified code here>```\n\n\
+    # PROBLEMATIC INPUT 1: <your first problematic input here>\n\n\
+    # PROBLEMATIC INPUT 2: <your second problematic input here>\n\n\
+    # PROBLEMATIC INPUT 3: <your third problematic input here>\n"
+    # )
 
 
 def extract_code_and_input(model_output: str):
 
     # Extract the problematic input using regex
-    match = re.search(r"PROBLEMATIC INPUT:\s*(.*)", model_output, re.DOTALL)
-    if match:
-        problematic_input = match.group(1).replace("```", "").strip()
+    pattern = re.compile(
+        r"PROBLEMATIC INPUT \d+:\n(.*?)(?=\nPROBLEMATIC INPUT \d+:|\Z)", re.DOTALL
+    )
+    problematic_inputs = [
+        input.replace("```", "").strip() for input in pattern.findall(model_output)
+    ]
+
+    # Extract the modified solution using regex
+    if problematic_inputs != []:
+        pattern = re.compile(
+            r"MODIFIED SOLUTION:\s*(.*?)\s*PROBLEMATIC INPUT 1:", re.DOTALL
+        )
     else:
-        problematic_input = None
-
-    # Remove the "PROBLEMATIC INPUT: ..." text from the original string
-    model_output = re.sub(
-        r"PROBLEMATIC INPUT:\s*.*", "", model_output, flags=re.DOTALL
-    ).strip()
-
-    # Extract the modified solution code
-    match = re.search(r"MODIFIED SOLUTION:\s*(.*)", model_output, re.DOTALL)
+        pattern = re.compile(r"MODIFIED SOLUTION:\s*(.*)", re.DOTALL)
+    match = pattern.search(model_output)
     if match:
         modified_solution = (
             match.group(1).replace("python", "").replace("```", "").strip()
@@ -115,40 +149,61 @@ def extract_code_and_input(model_output: str):
     ]
     safe = not any(func in modified_solution for func in dangerous_functions)
 
-    return modified_solution, problematic_input, safe
+    return modified_solution, problematic_inputs, safe
 
 
 def test_anti_solution(
     anti_solution: str,
     solution: str,
-    problematic_input: str,
+    problematic_inputs: List[str],
     datum: dict,
     max_correctness: float = 1.0,
+    debug: bool = False,
 ) -> bool:
 
-    # Generate a new output based on the problematic input and test the anti-solution
-    output = (
+    # Generate a new outputs based on the problematic inputs and test the anti-solution
+    outputs = (
         None
-        if problematic_input is None
+        if problematic_inputs == []
         else utils.check_correctness(
-            datum, solution, evaluate=problematic_input, timeout=10, debug=True
+            datum, solution, evaluate=problematic_inputs, timeout=10, debug=False
         )
     )
-    if output is not None:
-        anti_output = utils.check_correctness(
-            datum, anti_solution, evaluate=problematic_input, timeout=10, debug=True
+    if outputs is not None:
+        anti_outputs = utils.check_correctness(
+            datum, anti_solution, evaluate=problematic_inputs, timeout=10, debug=False
         )
     else:
-        anti_output = output
+        anti_outputs = outputs
+
+    def frac_matching_strings(list1, list2, debug):
+
+        if len(list1) != len(list2):
+            raise ValueError("Both lists must have the same length")
+        numer = 0
+        denom = 0
+        for i in range(len(list1)):
+            if list1[i] not in ["-1", "-2", -1, -2]:
+                denom += 1
+                if list1[i] == list2[i]:
+                    numer += 1
+
+        if debug:
+            print(f"{denom / len(list1):.2f} valid inputs")
+            if denom > 0:
+                print(f"{numer / denom:.2f} matching outputs")
+
+        return numer / denom if denom > 0 else 1
+
+    fraction_matching = frac_matching_strings(outputs, anti_outputs, debug=True)
 
     # Test the anti-solution against the correctness checks
-    results = utils.check_correctness(datum, anti_solution, timeout=10, debug=True)
+    results = utils.check_correctness(datum, anti_solution, timeout=10, debug=False)
     fraction_correct = float(sum(results) / len(results))
+    if debug:
+        print(f"{fraction_correct:.2f} checks passed")
 
-    return (
-        "\n".join(output) != "\n".join(anti_output)
-        or fraction_correct < max_correctness
-    )
+    return fraction_matching < 1 or fraction_correct < max_correctness
 
 
 def generate_anti_solutions(
@@ -197,8 +252,14 @@ def generate_anti_solutions(
             if not safe:
                 continue
             valid_anti_solution = test_anti_solution(
-                anti_solution, solution, problematic_input, datum
+                anti_solution, solution, problematic_input, datum, debug=True
             )
+
+        # Calculate the Levenshtein distance
+        levenshtein_distance = textdistance.levenshtein(solution, anti_solution)
+        normalised_levenstein_distance = levenshtein_distance / max(
+            len(solution), len(anti_solution)
+        )
 
         if attempts == 10:
             print("Failed to generate anti-solution")
